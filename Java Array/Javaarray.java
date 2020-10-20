a4eee7ab5b3e673c62d4e5db5ef3d1b7cce950e0
Javeria Tabassum <50498182+javeriatabassum145@users.noreply.github.com>
2020-10-21 02:39:57 +0500

    Revert "Used for loop and Updated Javaarray.java"

--- Java Array/Javaarray.java
+++ Java Array/Javaarray.java
@@ -5,16 +5,11 @@ class Javaarray {
       int[] age = {12, 4, 5, 2, 5};
    
       // access each array elements
-      for (int i = 0 ; i < age.length ; i++){
-        System.out.println("Element on index " + i + " is " + age[i]);
-      }
-
+      System.out.println("Accessing Elements of Array:");
+      System.out.println("First Element: " + age[0]);
+      System.out.println("Second Element: " + age[1]);
+      System.out.println("Third Element: " + age[2]);
+      System.out.println("Fourth Element: " + age[3]);
+      System.out.println("Fifth Element: " + age[4]);
     }
-}
-      //System.out.println("Accessing Elements of Array:");
-      //System.out.println("First Element: " + age[0]);
-      //System.out.println("Second Element: " + age[1]);
-      //System.out.println("Third Element: " + age[2]);
-      //System.out.println("Fourth Element: " + age[3]);
-      //System.out.println("Fifth Element: " + age[4]);
-   
+   }
